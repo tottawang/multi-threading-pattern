35a1553ef07fcda1c45c8fb0607378323fc9f5d1
Totta Wang <totta.wang@autodesk.com>
2017-07-05 04:34:48 +0800

    add documentation

--- src/com/sample/AtomicCounter/AtomicCounter.java
+++ src/com/sample/AtomicCounter/AtomicCounter.java
@@ -2,6 +2,12 @@ package com.sample.AtomicCounter;
 
 import java.util.concurrent.atomic.AtomicLong;
 
+/**
+ * Good reference http://mishadoff.com/blog/java-magic-part-4-sun-dot-misc-dot-unsafe/
+ * 
+ * @author wangto
+ *
+ */
 public class AtomicCounter implements Counter {
 
   private AtomicLong counter = new AtomicLong(0);
