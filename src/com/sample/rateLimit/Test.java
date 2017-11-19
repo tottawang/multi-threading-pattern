01937fa26541a1b38f8facded1a3b02f9fb8e15e
Totta Wang <totta.wang@autodesk.com>
2017-11-20 00:47:40 +0800

    add semaphore and rateLimit skeleton

---
+++ src/com/sample/rateLimit/Test.java
@@ -0,0 +1,25 @@
+package com.sample.rateLimit;
+
+import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.ScheduledFuture;
+import java.util.concurrent.TimeUnit;
+
+public class Test {
+
+  public static void main(String[] args) throws InterruptedException, ExecutionException {
+    ScheduledExecutorService scheduledExecutorService = Executors.newScheduledThreadPool(5);
+    ScheduledFuture scheduledFuture = scheduledExecutorService.schedule(new Callable() {
+      public Object call() throws Exception {
+        System.out.println("Executed!");
+        return "Called!";
+      }
+    }, 5, TimeUnit.SECONDS);
+
+    System.out.println("result = " + scheduledFuture.get());
+    scheduledExecutorService.shutdown();
+  }
+
+}
---
+++ src/com/sample/semaphore/Semaphore.java
@@ -0,0 +1,5 @@
+package com.sample.semaphore;
+
+public class Semaphore {
+
+}
