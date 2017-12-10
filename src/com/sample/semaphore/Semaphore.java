425985d45dce710ea423987f8af488c3b4f9232f
Totta Wang <totta.wang@autodesk.com>
2017-12-10 23:58:29 +0800

    update document

--- src/com/sample/semaphore/Semaphore.java
+++ src/com/sample/semaphore/Semaphore.java
@@ -4,9 +4,13 @@ package com.sample.semaphore;
 /**
  * http://tutorials.jenkov.com/java-concurrency/semaphores.html
  * 
+ * Implement a simple semaphore.
+ * 
  * @author wangto
  *
  */
 public class Semaphore {
+
+
 
 }
