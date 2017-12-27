6e2b600970bf9759aec1996642da1a57c7614106
Totta Wang <totta.wang@autodesk.com>
2017-12-27 15:50:44 +0800

    add semaphore implementation and test

--- src/com/sample/semaphore/Semaphore.java
+++ src/com/sample/semaphore/Semaphore.java
@@ -11,6 +11,33 @@ package com.sample.semaphore;
  */
 public class Semaphore {
 
+  private int permits = 0;
 
+  public Semaphore(int permits) {
+    this.permits = permits;
+  }
+
+  /**
+   * Blocking call to get available permit.
+   * 
+   * @throws InterruptedException
+   */
+  public synchronized void acquire() throws InterruptedException {
+    if (permits == 0) {
+      wait();
+    }
+    permits--;
+    System.out.println("acquired");
+  }
+
+  public synchronized void release() {
+    permits++;
+    notify();
+    System.out.println("released");
+  }
+
+  public int availablePermits() {
+    return permits;
+  }
 
 }
