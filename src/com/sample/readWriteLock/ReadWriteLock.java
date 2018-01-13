67aa62a1a0a31edca13c59a074bcd201fa75455e
Totta Wang <totta.wang@autodesk.com>
2018-01-13 17:24:32 +0800

    first cut implemenation

--- src/com/sample/readWriteLock/ReadWriteLock.java
+++ src/com/sample/readWriteLock/ReadWriteLock.java
@@ -1,5 +1,42 @@
 package com.sample.readWriteLock;
 
+/**
+ * http://tutorials.jenkov.com/java-concurrency/read-write-locks.html
+ * 
+ * @author wangtoumac
+ *
+ */
 public class ReadWriteLock {
+
+  private int writeCount;
+  private int readCount;
+
+  public synchronized void lockWrite() throws InterruptedException {
+    if (writeCount == 0 || readCount == 0) {
+      wait();
+    }
+    writeCount++;
+  }
+
+  public synchronized void unlockWrite() {
+    if (writeCount == 0) {
+      throw new RuntimeException("invalid write lock counter");
+    }
+    writeCount--;
+    if (writeCount == 0) {
+      notifyAll();
+    }
+  }
+
+  public synchronized void lockRead() throws InterruptedException {
+    if (writeCount != 0) {
+      wait();
+    }
+    readCount++;
+  }
+
+  public synchronized void unlockRead() {
+    readCount--;
+  }
 
 }
